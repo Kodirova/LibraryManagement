d55e90d20a8ed51bd01f01c69d03c45e80c88a31
Kimmy <kodirovairoda7@gmail.com>
2021-07-13 01:50:20 +0500

    heroku django

--- library/settings.py
+++ library/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'nu2xx00wvzho4c43c667jwdgflo1nph#^^$1a6=oip&97852m4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kimmy-library.herokuapp.com']
+ALLOWED_HOSTS = ['https://kimmy-library.herokuapp.com', 'kimmy-library.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
